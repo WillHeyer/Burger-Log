47e4b041207324cffc0b199fd2daa3cff87877c4
Will Heyer <williamhheyer@gmail.com>
2021-02-10 23:03:27 -0600

    bug fixing

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,4 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Triple-Stacked Avocado Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cali-Chicken Cheeseburger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Southern Style Veggie Burger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Pepper Jack Mushroom Burger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Tequila Lime Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Kyle Burger');
+INSERT INTO burgers (burger_name) VALUES ('Matt Burger');
+INSERT INTO burgers (burger_name) VALUES ('Hugh Burger');
+INSERT INTO burgers (burger_name) VALUES ('Will Burger');
